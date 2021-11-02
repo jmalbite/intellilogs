ba4c6d4e2bc028867bb62dc3b22b4af6ee86851f
jannmichael albite <albite.jannmicheal@intellicare.com.ph>
2021-11-02 17:50:50 +0800

    building client ui

--- client/src/components/VisitorLogs/VisitorAppbar.js
+++ client/src/components/VisitorLogs/VisitorAppbar.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import AppBar from '@mui/material/AppBar';
+import { AppBar, Toolbar } from '@mui/material';
+import intelilogs from '../../assets/intellilogs v2-01.png';
 
 const VisitorAppbar = () => {
   return (
     <>
-      <AppBar position="static">sample</AppBar>
+      <AppBar position="static" height="86px" color="primary">
+        <Toolbar>
+          <img
+            src={intelilogs}
+            alt="intelli-logs-logo"
+            style={{ width: '227px' }}
+          />
+        </Toolbar>
+      </AppBar>
     </>
   );
 };
