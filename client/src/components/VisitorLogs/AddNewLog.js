0da23fa74f023e20c111c990ef514b31cc96ff96
jannmichael albite <albite.jannmicheal@intellicare.com.ph>
2021-12-06 09:33:25 +0800

    pulled code from github (fully functional table with data in the table and new log form)

--- client/src/components/VisitorLogs/AddNewLog.js
+++ client/src/components/VisitorLogs/AddNewLog.js
@@ -48,7 +48,6 @@ const AddNewLog = () => {
   const [companySelected, setCompanySelected] = useState('');
   const [isShow, setIShow] = useState(false);
   const [isSign, setIsSigned] = useState(false);
-  const [otherCom, setOtherCom] = useState(false);
 
   //inserting visitor signature in postVisitorLog state
   useEffect(() => {
@@ -59,33 +58,9 @@ const AddNewLog = () => {
   }, [userSign]);
 
   useEffect(() => {
-<<<<<<< HEAD
-    if (errors.visitorname) setErrorName(true);
-    else setErrorName(false);
-
-    if (errors.company) setErrorCompany(true);
-    else setErrorCompany(false);
-
-    if (errors.otherCompany) setOtherCom(true);
-    else setOtherCom(false);
-
-    if (errors.area_to_visit) setErrorArea(true);
-    else setErrorArea(false);
-
-    if (errors.purpose) setErrorPurpose(true);
-    else setErrorPurpose(false);
-  }, [
-    errors.visitorname,
-    errors.company,
-    errors.area_to_visit,
-    errors.purpose,
-    errors.otherCompany,
-  ]);
-=======
     if (!!companySelected) setIShow(true);
     else setIShow(false);
   }, [companySelected]);
->>>>>>> eb03435a403428c763d59c671982d26ece91e0f6
 
   const handleClear = () => {
     reset({
@@ -98,19 +73,10 @@ const AddNewLog = () => {
     dispatch(clear_signature());
   };
 
-<<<<<<< HEAD
-  //handle area component
-  const handleArea = (e) => {
-    setPostVisitorlog({ ...postVisitorlog, area_visited: e.target.value });
-  };
-
-  const save = () => {
-=======
   const save = (data) => {
     let visitorsData = data;
     const signature = userSign;
     const time_visited = new Date();
->>>>>>> eb03435a403428c763d59c671982d26ece91e0f6
     if (!isSign) {
       visitorsData = { ...visitorsData, signature, time_visited };
       console.log(visitorsData);
@@ -128,83 +94,6 @@ const AddNewLog = () => {
           </Typography>
         </Grid>
 
-<<<<<<< HEAD
-            {/* ID NUMBER */}
-            <Grid item xs sm>
-              <TextField
-                {...register('id_number')}
-                autoComplete="off"
-                variant="outlined"
-                type="text"
-                label="ID number"
-                fullWidth
-                onChange={(e) =>
-                  setPostVisitorlog({
-                    ...postVisitorlog,
-                    id: e.target.value,
-                  })
-                }
-              />
-            </Grid>
-
-            {/* NAME */}
-            <Grid item xs sm>
-              <TextField
-                {...register('visitorname')}
-                autoComplete="off"
-                variant="outlined"
-                type="text"
-                label="Name"
-                error={errorName}
-                fullWidth
-                onChange={(e) =>
-                  setPostVisitorlog({ ...postVisitorlog, name: e.target.value })
-                }
-              />
-            </Grid>
-
-            {/* COMPANY */}
-            <Grid item xs sm>
-              <FormControl required fullWidth error={errorCompany}>
-                <InputLabel id="company">Company</InputLabel>
-                <Select
-                  {...register('company')}
-                  labelId="company"
-                  id="com"
-                  value={companySelected}
-                  label="Company"
-                  onChange={handleChangeCompany}
-                  required
-                >
-                  {companies.map((company) => (
-                    <MenuItem key={company} value={company}>
-                      {company}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Grid>
-
-            {/* OTHERS IF COMPANY IS SELECTED AS 'OTHERS' */}
-            {companySelected === 'OTHERS' ? (
-              <Grid item xs sm>
-                <TextField
-                  {...register('company')}
-                  error={otherCom}
-                  required
-                  name="company"
-                  variant="outlined"
-                  type="text"
-                  fullWidth
-                  label="Please input company"
-                  onChange={(e) =>
-                    setPostVisitorlog({
-                      ...postVisitorlog,
-                      company: e.target.value,
-                    })
-                  }
-                />
-=======
         {/* USER MUST SELECT COMPANY FIRST */}
         {!isShow ? (
           <Grid item xs sm alignSelf="center">
@@ -374,7 +263,6 @@ const AddNewLog = () => {
                     Clear
                   </Button>
                 </Grid>
->>>>>>> eb03435a403428c763d59c671982d26ece91e0f6
               </Grid>
             </form>
           ) : companySelected === 'OTHERS' ? (
@@ -399,29 +287,6 @@ const AddNewLog = () => {
                   />
                 </Grid>
 
-<<<<<<< HEAD
-            {/* AREA TO VISIT */}
-            <Grid item xs sm>
-              <FormControl fullWidth required error={errorArea}>
-                <InputLabel id="area-to-visit">Area</InputLabel>
-                <Select
-                  {...register('area_to_visit')}
-                  required
-                  labelId="area"
-                  id="area-to-visit"
-                  value={postVisitorlog.area_visited}
-                  onChange={handleArea}
-                  label="Area"
-                >
-                  {areas.map((area) => (
-                    <MenuItem key={area} value={area}>
-                      {area}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Grid>
-=======
                 <Grid item xs sm>
                   <Controller
                     name="visitorname"
@@ -440,7 +305,6 @@ const AddNewLog = () => {
                     )}
                   />
                 </Grid>
->>>>>>> eb03435a403428c763d59c671982d26ece91e0f6
 
                 {/* AREA TO VISIT */}
                 <Grid item xs sm>
